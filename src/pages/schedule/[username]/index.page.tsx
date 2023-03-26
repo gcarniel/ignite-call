4fe36652f2e00863ba4d72a287d8ae52499b303b
Gabriel Carniel <gpcarniel@outlook.com>
2023-03-26 15:59:16 -0300

    pagina de calendario e seus componentes iniciados

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,63 @@
+import { prisma } from '@/lib/prisma'
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { ScheduleForm } from './ScheduleForm'
+import { Container, UserHeader } from './style'
+
+interface ScheduleProps {
+  user: {
+    fullname: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} alt={user.fullname} />
+        <Heading>{user.fullname}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+
+      <ScheduleForm />
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  console.log(user)
+
+  return {
+    props: {
+      user: {
+        fullname: user.fullname,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
